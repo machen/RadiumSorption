97ceceb4fd514642f4b57ee7c8b4b47e5cd8222d
machen <machen27@gmail.com>
2016-04-21 14:10:41 -0400

    pyPHREEQC master table plotting
    Updated pyPHREEQC to pull data from master table, only pulling "vetted"
    data

--- Surface Complexation/PHREEQC Modeling/pyPHREEQC.py
+++ Surface Complexation/PHREEQC Modeling/pyPHREEQC.py
@@ -104,12 +104,11 @@ titleString = "Single site model, No Electrostatics, K1 = 0, K2 = -5.67, 0.1-10 
 #x = simulation({'totRa':totRa,'k1':k1,'k2':k2},[2,10],tmp,db)
 #x.generateData()
 sns.set_palette("deep",n_colors = 6)
-expData = extractData('.\RaFHY Experimental Data.xlsx')
-exp5 = expData.ix[expData['Total Activity']==5,:]
-exp10 = expData.ix[expData['Total Activity']==10,:]
-exp50 = expData.ix[expData['Total Activity']==50,:]
-exp100 = expData.ix[expData['Total Activity']==100,:]
-exp500 = expData.ix[expData['Total Activity']==500,:]
+
+#Find experimental data to use
+expData = extractData('..\..\Sorption Experiments\Sorption Experiment Master Table.xlsx')
+expData = expData.ix[expData.ix[:,'Include?']==True,:] #Select only data that's been vetted
+expData = expData.ix[expData.ix[:,'Mineral']=="Ferrihydrite"]
 
 f1 = plt.figure(num=1,figsize=(10,8))
 f1.clf()
@@ -117,9 +116,11 @@ ax = f1.add_subplot(111)
 
 labelStr = "1 site model, K1: {k1} K2: {k2} Sites (mol): {sites}"
 K2val = np.array([-5.67])
-K1val = np.arange(2.5,3.6,0.1)
+#K1val = np.arange(2.5,3.6,0.1)
+K1val = np.array([2])
 #siteMolVal = np.logspace(-3,0,num=4,endpoint=True, base=10)
-siteMolVal = np.arange(0.004,0.01,0.0005)
+#siteMolVal = np.arange(0.004,0.01,0.0005)
+siteMolVal = np.array([0.006])
 ncol = np.size(K1val)*np.size(K2val)*np.size(siteMolVal)
 cmap = sns.cubehelix_palette(n_colors=ncol,dark=0.3,rot=0.4,light=0.8,gamma=1.3)
 palette = itertools.cycle(cmap)
@@ -133,12 +134,25 @@ for K1 in K1val:
             ax.plot(simRes.ix[:,'pH'],simRes.ix[:,'fSorb'],'-',label=labelStr.format(k1=K1,k2=K2,sites=siteMol),color=next(palette))
             print K1,K2,siteMol
         
+#Plot all of the data without differentiation
+#expPlot = ax.errorbar(expData.ix[:,'pH'],expData.ix[:,'fSorb'],xerr=expData.ix[:,'spH'],yerr=expData.ix[:,'sfSorb'],fmt='o',label='Experimental Data')
 
-exp5Plot = ax.errorbar(exp5.ix[:,'pH'],exp5.ix[:,'fSorb'],xerr=exp5.ix[:,'spH'],yerr=exp5.ix[:,'sfSorb'],fmt='o',label='Experimental Data 5 Bq Total')
-exp10Plot = ax.errorbar(exp10.ix[:,'pH'],exp10.ix[:,'fSorb'],xerr=exp10.ix[:,'spH'],yerr=exp10.ix[:,'sfSorb'],fmt='o',label='Experimental Data 10 Bq Total')
-exp50Plot = ax.errorbar(exp50.ix[:,'pH'],exp50.ix[:,'fSorb'],xerr=exp50.ix[:,'spH'],yerr=exp50.ix[:,'sfSorb'],fmt='o',label='Experimental Data 50 Bq Total')
-exp100Plot = ax.errorbar(exp100.ix[:,'pH'],exp100.ix[:,'fSorb'],xerr=exp100.ix[:,'spH'],yerr=exp100.ix[:,'sfSorb'],fmt='o',label='Experimental Data 100 Bq Total')
-exp500Plot = ax.errorbar(exp500.ix[:,'pH'],exp500.ix[:,'fSorb'],xerr=exp500.ix[:,'spH'],yerr=exp500.ix[:,'sfSorb'],fmt='o',label='Experimental Data 500 Bq Total')
+exp5 = expData.ix[expData['Total Activity']==5,:]
+exp10 = expData.ix[expData['Total Activity']==10,:]
+exp50 = expData.ix[expData['Total Activity']==50,:]
+exp100 = expData.ix[expData['Total Activity']==100,:]
+exp500 = expData.ix[expData['Total Activity']==500,:]
+
+if not exp5.empty:
+    exp5Plot = ax.errorbar(exp5.ix[:,'pH'].values,exp5.ix[:,'fSorb'].values,xerr=exp5.ix[:,'spH'].values,yerr=exp5.ix[:,'sfSorb'].values,fmt='o',label='Experimental Data 5 Bq Total')
+if not exp10.empty:
+    exp10Plot = ax.errorbar(exp10.ix[:,'pH'].values,exp10.ix[:,'fSorb'].values,xerr=exp10.ix[:,'spH'].values,yerr=exp10.ix[:,'sfSorb'].values,fmt='o',label='Experimental Data 10 Bq Total')
+if not exp50.empty:
+    exp50Plot = ax.errorbar(exp50.ix[:,'pH'].values,exp50.ix[:,'fSorb'].values,xerr=exp50.ix[:,'spH'].values,yerr=exp50.ix[:,'sfSorb'].values,fmt='o',label='Experimental Data 50 Bq Total')
+if not exp100.empty:
+    exp100Plot = ax.errorbar(exp100.ix[:,'pH'].values,exp100.ix[:,'fSorb'].values,xerr=exp100.ix[:,'spH'].values,yerr=exp100.ix[:,'sfSorb'].values,fmt='o',label='Experimental Data 100 Bq Total')
+if not exp500.empty:
+    exp500Plot = ax.errorbar(exp500.ix[:,'pH'].values,exp500.ix[:,'fSorb'].values,xerr=exp500.ix[:,'spH'].values,yerr=exp500.ix[:,'sfSorb'].values,fmt='o',label='Experimental Data 500 Bq Total')
 
 
 ax.legend(loc=0)
@@ -147,46 +161,3 @@ ax.set_xlabel('pH')
 ax.set_ylabel('Fraction Sorbed')
 ax.set_ylim([-0.01,1.0])
 plt.show()
-    
-
-
-#Original script which focused on pulling in and plotting data from already produced data
-"""
-def extractData(path,PHREEQC=False):
-    #Function retrieves data from an excel spreadsheet, also includes the error
-    fileLoc = path
-    data = pd.read_excel(fileLoc)
-    return data
-
-#tetraMod = extractData('RaFHY_Sajih4SiteModel.xlsx')
-#SCM = extractData('RaFHY_SajihSCM.xlsx')
-SCM = extractData('RaEqTestOUTPUT.xlsx',PHREEQC=True)
-expData = extractData('RaFHY Experimental Data.xlsx')
-#testMod = extractData('RaFHY_SajihSCM_TEST.xlsx')
-exp5 = expData.ix[expData['Total Activity']==5,:]
-exp10 = expData.ix[expData['Total Activity']==10,:]
-exp50 = expData.ix[expData['Total Activity']==50,:]
-exp100 = expData.ix[expData['Total Activity']==100,:]
-exp500 = expData.ix[expData['Total Activity']==500,:]
-
-f1 = plt.figure(num=1,figsize=(10,8))
-plt.clf()
-ax = f1.add_subplot(111)
-#tetplot = ax.plot(tetraMod['pH'],tetraMod['fSorb'],'-',label='Tetradentate Model, Sajih')
-SCMplot = ax.plot(SCM.ix[:,'pH'],SCM.ix[:,'fSorb'],'-',label='Simple Complexation Model, Sajih constants, 1 site type')
-#testplot = ax.plot(testMod['pH'],testMod['fSorb'],'-',label='Simple Complexation Model, Weak log K: '+weakK+', Strong log K: '+strongK)
-#weakTest = ax.plot(testMod['pH'],testMod['fWeak'],'--',label='Weak sites')
-#strTest = ax.plot(testMod['pH'],testMod['fStrong'],'--',label='Strong Sites')
-exp5Plot = ax.errorbar(exp5.ix[:,'pH'],exp5.ix[:,'fSorb'],xerr=exp5.ix[:,'spH'],yerr=exp5.ix[:,'sfSorb'],fmt='o',label='Experimental Data 5 Bq Total') #Need to fix this coloring scheme
-exp10Plot = ax.errorbar(exp10.ix[:,'pH'],exp10.ix[:,'fSorb'],xerr=exp10.ix[:,'spH'],yerr=exp10.ix[:,'sfSorb'],fmt='o',label='Experimental Data 10 Bq Total')
-exp50Plot = ax.errorbar(exp50.ix[:,'pH'],exp50.ix[:,'fSorb'],xerr=exp50.ix[:,'spH'],yerr=exp50.ix[:,'sfSorb'],fmt='o',label='Experimental Data 50 Bq Total')
-exp100Plot = ax.errorbar(exp100.ix[:,'pH'],exp100.ix[:,'fSorb'],xerr=exp100.ix[:,'spH'],yerr=exp100.ix[:,'sfSorb'],fmt='o',label='Experimental Data 100 Bq Total')
-exp500Plot = ax.errorbar(exp500.ix[:,'pH'],exp500.ix[:,'fSorb'],xerr=exp500.ix[:,'spH'],yerr=exp500.ix[:,'sfSorb'],fmt='o',label='Experimental Data 500 Bq Total')
-ax.legend(loc=0)
-ax.set_title(str(datetime.date.today()))
-ax.set_xlabel('pH')
-ax.set_ylabel('Fraction Sorbed')
-ax.set_ylim([-0.01,1.0])
-plt.show()
-#f1.savefig('Radium Complexation All.pdf',dpi=900)
-"""
